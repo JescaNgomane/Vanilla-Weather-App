2c4f887791b283393ec3b0248f42e377f43ec1e0
JescaNgomane <ntavasingomane72@gmail.com>
2024-09-08 19:40:40 +0200

    Added javascript on the searchbar

--- source/weather.js
+++ source/weather.js
@@ -1,1 +1,9 @@
-alert("hi");
+function searchBar(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#input-search-form");
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#input-search");
+searchFormElement.addEventListener("submit", searchBar);
